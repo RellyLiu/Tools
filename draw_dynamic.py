20fc47ab6c1cc109528debabffeb0176a6a42d36
test <test@test.com>
2019-12-28 05:51:50 +0800

    modify draw tool

--- draw_dynamic.py
+++ draw_dynamic.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# coding=utf-8
-
 import numpy as np
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d
@@ -11,8 +8,7 @@ def onClick(event):
     global pause
     pause = True
 
-def plot_vehicle(x, y, theta, x_traj, y_traj):  # pragma: no cover
-    # Corners of triangular vehicle when pointing to the right (0 radians)
+def plot_vehicle(x, y, theta, x_traj, y_traj):
     p1_i = np.array([0.5, 0, 1]).T
     p2_i = np.array([-0.5, 0.25, 1]).T
     p3_i = np.array([-0.5, -0.25, 1]).T
@@ -37,7 +33,7 @@ plt.ion()
 fig = plt.figure(1)
 t = [0]
 t_now = 0
-f = open("/home/dahua/workspace/script/enml_data")
+f = open("/home/dahua/workspace/script/pose_data")
 x = []
 y = []
 a = []
@@ -59,31 +55,15 @@ for line in f:
     x.append( float(data[0] ) )
     y.append( float(data[1] ) ) 
     a.append( float(data[2] ) ) 
-    b = "x "
-    b=b+str(x[-1]) 
-    c = " y "
-    c=c+str(y[-1]) 
-    d = " a "
-    d=d+str(a[-1])
-    e = b+c+d
-    #plt.plot(x,y,linewidth='1',linestyle='-',color='r',marker='>',markersize=5)
- #   plt.plot(x[-1],y[-1],linewidth='1',linestyle='-',color='b',marker='>',markersize=10,label=d)
     plot_vehicle(x[-1], y[-1], a[-1], x, y)
-    plt.plot(x, y, 'b--',label=e)
+    plt.plot(x, y, 'b--')
     ax = fig.add_subplot( 111)
     ax.axis('equal')
- #   pdb.set_trace()
- #   plt.legend(loc='best')
     plt.legend(loc='lower left')
     plt.text(-3, 20, "b",size = 15 ,alpha = 0.2)
     plt.draw()
     plt.pause(0.000000001)
 plt.ioff()
-#fig = plt.figure()
-#plt.plot(x,y,linewidth='1',linestyle='-',color='r',marker='>',markersize=8)
-#ax = fig.add_subplot( 111)
-#ax.axis('equal')
-
 plt.show()
 
 
